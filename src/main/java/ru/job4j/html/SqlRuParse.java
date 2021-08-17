c5dd5aca02da577307ae60549f8a4f3198ded103
DashaPC <miss.sinkova-dasha@yandex.ru>
2021-08-17 23:42:59 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -12,18 +12,20 @@ import java.util.List;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        List<Element> row = doc.select(".postslisttopic").subList(0, 5); //полечаем нужный тег
-        int count = 0;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
+        for (int page = 1; page <= 5; page++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            Elements row = doc.select(".postslisttopic");
+            int count = 0;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
             System.out.println(href.text());
             System.out.println(doc.getElementsByAttribute("style").select(".altCol").get(count).text());
             count++;
             SqlRuDateTimeParser date = new SqlRuDateTimeParser();
             System.out.println("Дата " + date.parse(td.parent().child(5).text()).format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm")));
             System.out.println();
+            }
         }
     }
 }
