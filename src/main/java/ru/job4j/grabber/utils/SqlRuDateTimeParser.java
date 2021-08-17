a3edd6103b3976f737d24f19f85f300b9a03c46e
DashaPC <miss.sinkova-dasha@yandex.ru>
2021-08-17 22:22:58 +0300

    2.1. Преобразование даты [#289476 #184080]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -3,29 +3,31 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
+import java.util.Map;
+import static java.util.Map.entry;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+    private static final Map<String, String> MONTHS = Map.ofEntries(entry("май", "may"), entry("янв", "jan"), entry("фев", "feb"), entry("мар", "mar"), entry("апр", "apr"), entry("июн", "jun"), entry("июл", "jul"), entry("авг", "Aug"), entry("сен", "sep"), entry("окт", "oct"), entry("ноя", "nov"), entry("дек", "dec"));
     @Override
     public LocalDateTime parse(String parse) {
         String date = parse.split(",")[0];
         String time = parse.split(",")[1];
         if (date.contains("сегодня")) {
-            date = LocalDate.now().format(DateTimeFormatter.ofPattern("d MMM yy"));
+            date = LocalDate.now().format(DateTimeFormatter.ofPattern("d MMM yy", Locale.ENGLISH));
         } else if (date.contains("вчера")) {
-            date = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("d MMM yy"));
+            date = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("d MMM yy", Locale.ENGLISH));
         } else {
             String[] dateArr = date.split(" ");
-            if (dateArr[1].equals("май")) {
-                dateArr[1] = "мая";
-            } else {
-                dateArr[1] = dateArr[1] + ".";
-            }
+            dateArr[1] = MONTHS.get(dateArr[1]);
             date = dateArr[0] + " " + dateArr[1] + " " + dateArr[2];
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yy HH:mm").withLocale(new Locale("ru"));
-        String datetime = date + "" + time;
-        LocalDateTime res = LocalDateTime.parse(datetime, formatter);
-        return res;
+        System.out.println(date);
+        DateTimeFormatterBuilder formatterBuilder = new DateTimeFormatterBuilder();
+        formatterBuilder.parseCaseInsensitive();
+        formatterBuilder.appendPattern("d MMM yy HH:mm");
+        DateTimeFormatter formatter = formatterBuilder.toFormatter().withLocale(Locale.ENGLISH);
+        return LocalDateTime.parse(date + "" + time, formatter);
     }
 }
